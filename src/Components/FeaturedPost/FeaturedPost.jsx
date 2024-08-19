ffbaada6453bbc64c94ce608e7f230d31c3d823e
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-19 14:22:51 +0600

    Alhamdulilah banner updated

--- src/Components/FeaturedPost/FeaturedPost.jsx
+++ src/Components/FeaturedPost/FeaturedPost.jsx
@@ -21,6 +21,7 @@ const FeaturedPost = () => {
           spaceBetween={30}
           navigation={true}
           loop={true}
+           className='!h-[400px] !w-full'
           modules={[Navigation]}
           breakpoints={{
             1024: {
@@ -38,22 +39,8 @@ const FeaturedPost = () => {
           <SwiperSlide>
             <div className='flex flex-col items-center'>
               <div className="relative mb-4">
-                <div className="bg-[#7EC1F7] flex justify-center items-center rounded-full w-[200px] h-[240px]">
-                  <img src="/src/assets/image/Featured/1.png" alt="Puppy 1" className='w-[140px] h-[180px]' />
-                </div>
-                <div className="absolute bottom-0 left-0 right-0 flex justify-between items-center h-[50px]">
-                  <div className="w-[60px] h-[60px] border-b-4 border-[#7EC1F7] rounded-bl-full"></div>
-                  <div className="w-[60px] h-[60px] border-b-4 border-[#7EC1F7] rounded-br-full"></div>
-                </div>
-              </div>
-              <h1 className='font-bold text-black text-2xl text-center'>German Breed <br /> Alsatian Puppies</h1>
-            </div>
-          </SwiperSlide>
-                    <SwiperSlide>
-            <div className='flex flex-col items-center'>
-              <div className="relative mb-4">
-                <div className="bg-[#7EC1F7] flex justify-center items-center rounded-full w-[200px] h-[240px]">
-                  <img src="/src/assets/image/Featured/1.png" alt="Puppy 1" className='w-[140px] h-[180px]' />
+                <div className="bg-[#7EC1F7] flex justify-center items-center rounded-full !w-[200px] !h-[240px]">
+                  <img src="/src/assets/image/Featured/1.png" alt="Puppy 1" className='!w-[140px] !h-[180px]' />
                 </div>
                 <div className="absolute bottom-0 left-0 right-0 flex justify-between items-center h-[50px]">
                   <div className="w-[60px] h-[60px] border-b-4 border-[#7EC1F7] rounded-bl-full"></div>
@@ -68,8 +55,8 @@ const FeaturedPost = () => {
           <SwiperSlide>
             <div className='flex flex-col items-center'>
               <div className="relative mb-4">
-                <div className="bg-[#FE8E68] flex justify-center items-center rounded-full w-[200px] h-[240px]">
-                  <img src="/src/assets/image/Featured/2.png" alt="Puppy 2" className='w-[140px] h-[180px]' />
+                <div className="bg-[#FE8E68] flex justify-center items-center  rounded-full !w-[200px] !h-[240px]">
+                  <img src="/src/assets/image/Featured/2.png" alt="Puppy 2" className='!w-[140px] !h-[180px] !object-fill' />
                 </div>
                 <div className="absolute top-0 left-0 right-0 flex justify-between items-center h-[20px]">
                   <div className="w-[70px] h-[70px] border-t-4 border-[#FE8E68] rounded-tl-full"></div>
@@ -84,8 +71,8 @@ const FeaturedPost = () => {
           <SwiperSlide>
             <div className='flex flex-col items-center'>
               <div className="relative mb-4">
-                <div className="bg-[#FBB742] flex justify-center items-center rounded-full w-[200px] h-[240px]">
-                  <img src="/src/assets/image/Featured/3.png" alt="Puppy 3" className='w-[140px] h-[180px]' />
+                <div className="bg-[#FBB742] flex justify-center items-center rounded-full !w-[200px] !h-[240px]">
+                  <img src="/src/assets/image/Featured/3.png" alt="Puppy 3" className='!w-[140px] !h-[180px]' />
                 </div>
                 <div className="absolute bottom-0 left-0 right-0 flex justify-between items-center h-[50px]">
                   <div className="w-[60px] h-[60px] border-b-4 border-[#FBB742] rounded-bl-full"></div>
@@ -98,8 +85,8 @@ const FeaturedPost = () => {
           <SwiperSlide>
             <div className='flex flex-col items-center'>
               <div className="relative mb-4">
-                <div className="bg-[#38C5D9] flex justify-center items-center rounded-full w-[200px] h-[240px]">
-                  <img src="/src/assets/image/Featured/4.png" alt="Puppy 4" className='w-[140px] h-[180px]' />
+                <div className="bg-[#38C5D9] flex justify-center items-center rounded-full !w-[200px] !h-[240px]">
+                  <img src="/src/assets/image/Featured/4.png" alt="Puppy 4" className='!w-[140px] !h-[180px] !object-fill' />
                 </div>
                 <div className="absolute top-0 left-0 right-0 flex justify-between items-center h-[20px]">
                   <div className="w-[60px] h-[60px] border-t-4 border-[#38C5D9] rounded-tl-full"></div>
@@ -107,6 +94,20 @@ const FeaturedPost = () => {
                 </div>
               </div>
               <h1 className='font-bold text-black text-2xl text-center'>German Breed <br /> Alsatian Puppies</h1>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='flex flex-col items-center'>
+              <div className="relative mb-4">
+                <div className="bg-[#FBB742] flex justify-center items-center rounded-full !w-[200px] !h-[240px]">
+                  <img src="/src/assets/image/Featured/3.png" alt="Puppy 3" className='!w-[140px] !h-[180px]' />
+                </div>
+                <div className="absolute bottom-0 left-0 right-0 flex justify-between items-center h-[50px]">
+                  <div className="w-[60px] h-[60px] border-b-4 border-[#FBB742] rounded-bl-full"></div>
+                  <div className="w-[60px] h-[60px] border-b-4 border-[#FBB742] rounded-br-full"></div>
+                </div>
+              </div>
+              <h1 className='font-bold text-black text-2xl text-center'>Winter Clothing <br /> for Puppies</h1>
             </div>
           </SwiperSlide>
 
