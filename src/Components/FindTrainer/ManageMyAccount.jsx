d7f839f8b656238fb2ce88bbb7258498651e7480
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-19 16:36:12 +0600

    Alhamdulillah updated

--- src/Components/FindTrainer/ManageMyAccount.jsx
+++ src/Components/FindTrainer/ManageMyAccount.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../AllCart/Banner';
 
 const ManageMyAccount = () => {
     return (
         <div>
-            
+           <Banner></Banner> 
         </div>
     );
 };
