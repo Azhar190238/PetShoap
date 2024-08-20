44ed69144a81875d701a3a4b9e7538f08f2cd104
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-20 10:51:59 +0600

    Alhamdulillah updated

--- src/Components/FindTrainer/ManageMyAccount.jsx
+++ src/Components/FindTrainer/ManageMyAccount.jsx
@@ -69,9 +69,9 @@ const ManageMyAccount = () => {
                             </div>
                         </div>
                         <div className='border-2 border-red-400 my-10'></div>
-                        <div className='flex justify-between'>
+                        <div className='flex flex-col md:flex-row justify-between my-4'>
                             <div className='flex space-x-4'>
-                                <button className="btn btn-outline  border-red-400 bg-[#FF5C2C] text-red-400 rounded-3xl text-xl">Contact Me</button>
+                                <button className="btn btn-outline  border-red-400 bg-[#FF5C2C] rounded-3xl text-white text-xl">Contact Me</button>
                                 <button className="btn btn-outline border-red-400 text-red-400 rounded-3xl text-xl">Get a Quote </button>
                             </div>
                             <div>
@@ -79,11 +79,41 @@ const ManageMyAccount = () => {
                             </div>
                         </div>
                         <div className='bg-[#FF5C2C1c] my-10 rounded-3xl '>
-                           <div className=' p-16 text-center flex flex-col space-y-5'> 
-                            <h1 className='text-3xl font-bold text-[#FF5C2C]'>About Me</h1>
-                            <h2 className='text-2xl'>IN BUSINESS SINCE 2014</h2>
-                            <p className='text-lg'>We are a reward-based training company that offers private in-home training, as well as, group classes at The Pooch Palace in the beautiful, Alabama town of Cullman. For more information about us, visit our website, NamasteDogTraining.com, or find us on Facebook, Instagram, Tik Tok, and Youtube!</p></div>
+                            <div className=' p-16 text-center flex flex-col space-y-5'>
+                                <h1 className='text-3xl font-bold text-[#FF5C2C]'>About Me</h1>
+                                <h2 className='text-2xl'>IN BUSINESS SINCE 2024</h2>
+                                <p className='text-lg'>We are a reward-based training company that offers private in-home training, as well as, group classes at The Pooch Palace in the beautiful, Alabama town of Cullman. For more information about us, visit our website, NamasteDogTraining.com, or find us on Facebook, Instagram, Tik Tok, and Youtube!</p></div>
+                            <div className=' p-16 text-center flex flex-col space-y-5'>
+                                <h1 className='text-3xl font-bold text-[#FF5C2C]'>HOURS OF OPERATION</h1>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                                <h2 className='text-2xl'>Tue: 8:30AM - 5:00PM</h2>
+                            </div>
+                            <div className='border-2 border-red-400 mx-16 my-10'></div>
+                            <div className=' p-16 text-center flex flex-col space-y-5'>
+                                <h1 className='text-3xl font-bold text-[#FF5C2C]'>My Services</h1>
+                            </div>
+
                         </div>
+
+                        <div className='bg-[#FF5C2C1c] my-10 rounded-3xl '>
+                            <div className=' p-16 text-center flex flex-col space-y-5'>
+                                <h1 className='text-3xl font-bold text-[#FF5C2C]'>Contact</h1>
+                              
+                                <p className='text-lg'>249 Upper Bailey RoadNewport, <br /> PA 17074 <br />
+                                +1 456 245 4632</p>
+
+
+
+                            </div>
+
+                        </div>
+
+
+
                     </div>
                 </div>
             </div>
