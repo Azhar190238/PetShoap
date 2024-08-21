00d503a6ff3235b10efe06491e85586869801adb
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-21 12:52:11 +0600

    Alhamdulillah updated

--- src/Components/singleShop/SingleShop.jsx
+++ src/Components/singleShop/SingleShop.jsx
@@ -14,7 +14,7 @@ import React, { useRef, useState } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
